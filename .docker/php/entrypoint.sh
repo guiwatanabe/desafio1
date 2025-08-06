6078ab9c67239fb529b8a7088b3570587d0c307c
Guilherme Watanabe <guiwatanabe@hotmail.com>
2025-08-06 11:21:29 -0300

    Commit inicial - Laravel, Docker

---
+++ .docker/php/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+# Set permissions for Laravel directories
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+
+# permissions for PHPMyAdmin
+mkdir -p /sessions
+
+chmod 777 /sessions
+
+php artisan migrate --force
+php artisan config:cache
+php artisan route:cache
+
+exec "$@"
